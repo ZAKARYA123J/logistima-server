4e1221f43d6210315fc00a9288e0fe358ae89894
ZAKARYA123J <zakaryabaouali255@gmail.com>
2026-01-06 15:15:49 +0100

    redis configuration

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
-import express from "express"
-const PORT=3000
-const app = express()
-import {sequelize} from "./config/database.js"
+import express from "express";
+import redis from "redis";
+
+const PORT = 3000;
+const app = express();
+import { sequelize } from "./config/database.js";
+
+const redisUrl = 'redis://localhost:6379';
+const client = redis.createClient({ url: redisUrl });
+
+client.on("error", (error) => {
+    console.error("Redis ERROR***", error);
+});
+
+client.on("connect", () => {
+    console.log("Redis connected.");
+});
+
+(async () => {
+    await client.connect();
+})();
 
 async function bootstrap() {
     const MAX_RETRIES = 20;
     const RETRY_DELAY = 5000; // 5 seconds
-
+    
     for (let i = 0; i < MAX_RETRIES; i++) {
         try {
             await sequelize.authenticate();
-            console.log('Database authenticate');
+            console.log('Database authenticated');
             return; // Connection successful
         } catch (err) {
             console.error(`Database connection failed (attempt ${i + 1}/${MAX_RETRIES})`);
@@ -22,7 +39,9 @@ async function bootstrap() {
         }
     }
 }
-bootstrap()
-app.listen(PORT,()=>{
-    console.log(`Server is Runing on ${PORT}`)
-})
+
+bootstrap();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
