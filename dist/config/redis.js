f7b225a0127db45072c4ed2e06cb717eb1705188
zakaria <zakaryabaouali255@gmail.com>
2026-01-08 12:38:51 +0100

    new

--- dist/config/redis.js
+++ dist/config/redis.js
@@ -1,4 +1,4 @@
-const REDIS_URL = "redis://localhost:6379";
-const REDIS_PASSWORD;
- || undefined;
 export {};
+// import { createClient,RedisClientType } from "redis";
+// const REDIS_URL="redis://localhost:6379"
+// const REDIS_PASSWORD:process.env.password || undefined
